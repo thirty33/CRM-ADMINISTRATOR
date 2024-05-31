6b7fe57a925498365ea1d86bdee2dbaa5bd04d99
joelSuarez <joel.suarez@linktic.co>
2024-05-30 23:56:30 -0400

    table search input finished, create custom form component for text input

--- resources/js/Components/CustomForm.vue
+++ resources/js/Components/CustomForm.vue
@@ -1,76 +1,27 @@
 <script setup lang="ts">
 import _ from "lodash";
+import { CustomisableForm } from "@/Classes/Forms";
 
-import InputError from "@/Components/InputError.vue";
-import InputLabel from "@/Components/InputLabel.vue";
-import TextInput from "@/Components/TextInput.vue";
-import CustomTextInputForm from "@/Components/CustomFormInputs/CustomTextInputForm.vue";
-import { InputForm } from "@/Classes/Forms";
-
-import { reactive, watch, inject, computed, ref } from "vue";
-import { useForm, router, usePage } from "@inertiajs/vue3";
-import { useTranslation } from "@/Hooks/useTranslations";
-
-const page = usePage();
-const errors = computed(() => page.props.errors)
-
-const getPage = inject("getPageProgramatically") as ({}) => void;
-const getQueryParams = inject("transformQueryParamsFromRoute") as () => {
-  [key: string]: string;
-};
-
-let queryParams = getQueryParams();
-
-const form = reactive({
-  q: queryParams["Q"] ?? "",
-});
-
-function submit() {}
-
-const debounceSearch = _.debounce(() => {
-  getPage({
-    q: form.q,
-    page: 1,
-  });
-}, 1000);
-
-watch(
-  () => form.q,
-  () => {
-    debounceSearch();
-  }
-);
-
-const input: InputForm = {
-  labelName: "Search",
-  inputName: "search",
-  inputType: "text",
-  required: false,
-  autofocus: true,
-  autocomplete: "search",
-  error: errors.value?.q ?? '',
-  formProp: 'q',
-  typeInput: CustomTextInputForm
-};
-
-const InputForms: InputForm[] = [
-  input
-];
+const props = defineProps<{
+  form: CustomisableForm,
+}>();
 
 </script>
 
 <template>
   <form
-    @submit.prevent="submit"
+    @submit.prevent="$emit('submit')"
     class="mt-6 space-y-6 grid grid-cols-4 gap-x-0.5"
   >
-    <component
-      v-for="input in InputForms"
-      :is="input.typeInput"
-      :key="input.inputName"
-      :input="input"
-      :prop="input.formProp"
-      v-model="form"
-    />
+    <template v-for="input in props.form.inputFormList">
+      <component
+        v-if="input.typeInput"
+        :is="input.typeInput"
+        :key="input.inputName"
+        :input="input"
+        :prop="input.formProp"
+        v-model="props.form.formState"
+      />
+    </template>
   </form>
 </template>
--- resources/js/Components/WrapperForm.vue
+++ resources/js/Components/WrapperForm.vue
@@ -1,7 +1,53 @@
 <script setup lang="ts">
+import _ from "lodash";
+import { inject, computed } from "vue";
+import { usePage } from "@inertiajs/vue3";
+
 import CustomForm from '@/Components/CustomForm.vue';
+import CustomTextInputForm from "@/Components/CustomFormInputs/CustomTextInputForm.vue";
+import { InputFormBuilder, CustomisableForm } from "@/Classes/Forms";
+
+const page = usePage();
+const errors = computed(() => page.props.errors);
+
+const getPage = inject("getPageProgramatically") as ({}) => void;
+const getQueryParams = inject("transformQueryParamsFromRoute") as () => {
+  [key: string]: string;
+};
+
+let queryParams = getQueryParams();
+
+const form = new CustomisableForm({
+  q: queryParams["Q"] ?? "",
+});
+
+form.inputFormList = [
+  InputFormBuilder.instance
+    .setLabel("Search")
+    .setName("search")
+    .setType("text")
+    .setRequired(false)
+    .setAutofocus(true)
+    .setAutocomplete("search")
+    .setError(errors.value?.q ?? "")
+    .setFormProp("q")
+    .setTypeInput(CustomTextInputForm)
+    .getItem(),
+];
+
+const debounceSearch = _.debounce(() => {
+  getPage({
+    q: form.formState.q,
+    page: 1,
+  });
+}, 1000);
+
+form.setWatcherToField('q', () => debounceSearch());
+
+function submit() {}
+
 </script>
 
 <template>
-  <CustomForm />
+  <CustomForm :form="form" @submit="submit"/>
 </template>
